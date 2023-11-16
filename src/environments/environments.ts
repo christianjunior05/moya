ee44bd6ed2d7df7816cbe6317278fbf1b5dbabce
christianjunior05 <bchrist.junior@gmail.com>
2023-11-16 11:17:06 +0000

    add

---
+++ src/environments/environments.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production : false ,
+     firebase : {
+        apiKey: "AIzaSyAb2dEhfUk-W-Brfq01V3phG4gJ4bnu2iw",
+        authDomain: "moya-1e6dc.firebaseapp.com",
+        projectId: "moya-1e6dc",
+        storageBucket: "moya-1e6dc.appspot.com",
+        messagingSenderId: "712312721612",
+        appId: "1:712312721612:web:de7b7964f238f1d05eb269",
+        measurementId: "G-XF2SEP83J5"
+      }
+};
